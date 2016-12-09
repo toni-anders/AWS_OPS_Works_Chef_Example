315c44bb4739dfeef74e242445007bee9fa38dc1
toni-anders <anderst@fh-brandenburg.de>
2016-12-09 13:04:49 +0100

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'sample'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures sample'
+long_description 'Installs/Configures sample'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/sample/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/sample' if respond_to?(:source_url)
